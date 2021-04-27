72b041ee4d51465be4174c2a3b9150e6ca597754
sherzod38 <72493786+sherzod38@users.noreply.github.com>
2021-04-27 12:13:42 +0500

    f3

--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import imghome from '../../assets/images/Group.png'
+// import { Link } from 'react-router-dom';
+// import imghome from '../../assets/images/Group.png'
 
 import DataBaza from '../../dataBaza/DataBaza';
 
